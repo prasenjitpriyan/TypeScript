0d3abd6a6fcdacee45b5b4480b4f5341232f4340
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-07-11 07:48:21 +0530

    Counter deleted

--- 08-basic/src/main.ts
+++ 08-basic/src/main.ts
@@ -1,16 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hello World!
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
